8ad62b989a41520525b1f440d0aac9f636c98c12
Firef1ghter <nagendrahada1609@gmail.com>
2022-05-08 20:24:07 +0530

    todolist

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Todo from "./To_Do";
+// import Counter from "./Counter/Counter" ;
+
+// var style1 = {textAlign:'center', marginLeft:'200px',width:'500px'}
+
+
+function App() {
+  return (
+    
+     <>
+    <div style={{ margin:'auto',height:'auto',width:'100%'}}>
+       <Todo></Todo>
+    </div>
+    </>
+   
+   
+  );
+}
+
+export default App;
